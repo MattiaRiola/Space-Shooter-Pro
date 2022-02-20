0786e39329f45c3bb85e6f5f544aba1da5d8b378
MattiaRiola <s280169@studenti.polito.it>
2022-02-20 20:43:01 +0100

    refactor: laser shooter

--- Assets/Scripts/Player.cs
+++ Assets/Scripts/Player.cs
@@ -7,39 +7,49 @@ public class Player : MonoBehaviour
 
     [SerializeField]
     private Vector3 _speed;
-    public Vector2 sensibility; 
+    public Vector2 sensibility;
     [SerializeField]
-        private GameObject _laserPrefab;
+    private GameObject _laserPrefab;
     private float _offset = 0.8f;
+
+    [SerializeField]
+    private float _fireRate = 0.5f;
+
+    private float _lastShotTime = 0.0f;
 
     // Start is called before the first frame update
     void Start()
     {
-        
-        sensibility = new Vector2(5,5);
-        _speed = new Vector3(0,0,0);
-        transform.position = new Vector3(0,0,0);
+        sensibility = new Vector2(5, 5);
+        _speed = new Vector3(0, 0, 0);
+        transform.position = new Vector3(0, 0, 0);
     }
 
     // Update is called once per frame
     void Update()
     {
-
-
-
         movementController();
-
-        if(Input.GetKeyDown(KeyCode.Space)){
-            Instantiate(
-                _laserPrefab,
-                transform.position+(Vector3.up*_offset),
-                Quaternion.identity
-            );
-        }
-        
+        if(Input.GetKeyDown(KeyCode.Space) && canShoot())
+            fireLaser();    
     }
 
-    void movementController(){
+
+    void fireLaser(){
+            _lastShotTime = Time.time;
+            Instantiate(
+                _laserPrefab,
+                transform.position + (Vector3.up * _offset),
+                Quaternion.identity
+            );
+    }
+    
+    // return true if the player can shoot false otherwise
+    public bool canShoot()
+    {
+        return Time.time - _lastShotTime > _fireRate;
+    }
+    void movementController()
+    {
 
         float horizontalInput = Input.GetAxis("Horizontal");
         float verticalInput = Input.GetAxis("Vertical");
@@ -48,42 +58,48 @@ public class Player : MonoBehaviour
         _speed.x = horizontalInput * sensibility.x;
         _speed.y = verticalInput * sensibility.y;
 
-        transform.Translate( _speed * Time.deltaTime);
+        transform.Translate(_speed * Time.deltaTime);
 
         xSpeedAndPositionUpdate(Input.GetAxis("Horizontal"));
-        
+
         ySpeedAndPositionUpdate(Input.GetAxis("Vertical"));
-        
+
     }
 
-    void xSpeedAndPositionUpdate(float horizontalInput){
-        if(transform.position.x >= MainCamera.CAMERA_LIMIT_VIEW.x){
+    void xSpeedAndPositionUpdate(float horizontalInput)
+    {
+        if (transform.position.x >= MainCamera.CAMERA_LIMIT_VIEW.x)
+        {
             //teleport object
-            transform.position = 
+            transform.position =
                 new Vector3(
-                    - MainCamera.CAMERA_LIMIT_VIEW.x+0.5f,
+                    -MainCamera.CAMERA_LIMIT_VIEW.x + 0.5f,
                     transform.position.y,
                     transform.position.z);
-            
+
         }
-        else if(transform.position.x <= - MainCamera.CAMERA_LIMIT_VIEW.x){
+        else if (transform.position.x <= -MainCamera.CAMERA_LIMIT_VIEW.x)
+        {
             //teleport object
-            transform.position = 
+            transform.position =
                 new Vector3(
-                    MainCamera.CAMERA_LIMIT_VIEW.x-0.5f,
+                    MainCamera.CAMERA_LIMIT_VIEW.x - 0.5f,
                     transform.position.y,
                     transform.position.z);
-            
+
         }
     }
-    void ySpeedAndPositionUpdate(float verticalInput){
-        if(transform.position.y >= MainCamera.CAMERA_LIMIT_VIEW.y){
+    void ySpeedAndPositionUpdate(float verticalInput)
+    {
+        if (transform.position.y >= MainCamera.CAMERA_LIMIT_VIEW.y)
+        {
             //set the speed to 0 if moving upward
             _speed.y = verticalInput <= 0 ? verticalInput : 0;
         }
-        else if(transform.position.y <= - MainCamera.CAMERA_LIMIT_VIEW.y){
+        else if (transform.position.y <= -MainCamera.CAMERA_LIMIT_VIEW.y)
+        {
             //set the speed to 0 if moving downward
-            _speed.y = verticalInput >= 0 ? verticalInput : 0;    
+            _speed.y = verticalInput >= 0 ? verticalInput : 0;
         }
         //stall position
         transform.position = new Vector3(
