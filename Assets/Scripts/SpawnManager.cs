501b4ebb1948bfca3821b139e7bab6537634db7e
MattiaRiola <s280169@studenti.polito.it>
2022-03-02 10:28:22 +0100

    feat: powerup animation and spawn method (Lesson 73)

--- Assets/Scripts/SpawnManager.cs
+++ Assets/Scripts/SpawnManager.cs
@@ -9,13 +9,19 @@ public class SpawnManager : MonoBehaviour
     private GameObject _enemyPrefab;
 
     [SerializeField]
+    private GameObject _tripleshotPowerupPrefab;
+    
+    [SerializeField]
     private GameObject _enemyContainer;
+    
     [SerializeField]
     private bool _spawnEnabled = true;
+
     // Start is called before the first frame update
     void Start()
     {
-        StartCoroutine(spawnRoutine());
+        StartCoroutine(spawnEnemyRoutine());
+        StartCoroutine(spawnPowerupRoutine());
     }
 
     // Update is called once per frame
@@ -25,7 +31,7 @@ public class SpawnManager : MonoBehaviour
     }
 
 
-    IEnumerator spawnRoutine()
+    IEnumerator spawnEnemyRoutine()
     {
         while (_spawnEnabled == true)
         {
@@ -37,6 +43,20 @@ public class SpawnManager : MonoBehaviour
                 );
             newEnemy.transform.parent = _enemyContainer.transform;
             yield return new WaitForSeconds(5.0f);
+        }
+    }
+    IEnumerator spawnPowerupRoutine()
+    {
+        while (_spawnEnabled == true)
+        {
+
+            GameObject newPowerup = Instantiate(
+                _tripleshotPowerupPrefab,
+                new Vector3(MainCamera.randomX(2), MainCamera.CAMERA_LIMIT_VIEW.y, 0),
+                Quaternion.identity
+                );
+            newPowerup.transform.parent = _enemyContainer.transform;
+            yield return new WaitForSeconds(Random.Range(3.0f,7.0f));
         }
     }
 
