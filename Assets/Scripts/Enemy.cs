b3e5c6c9458fde5ff1d35671b3db57740a283923
MattiaRiola <s280169@studenti.polito.it>
2022-03-07 15:12:07 +0100

    feat: enemy explosion animation lesson 108

--- Assets/Scripts/Enemy.cs
+++ Assets/Scripts/Enemy.cs
@@ -10,11 +10,20 @@ public class Enemy : MonoBehaviour
     private int _points = 10;
 
 
+    private Animator _anim;
+
+    private bool _isDestroyed = false;
     Player _player;
     // Start is called before the first frame update
     void Start()
     {
         _player = GameObject.FindWithTag("Player").GetComponent<Player>();
+        if (_player == null)
+            Debug.LogError("Player is null");
+
+        _anim = this.gameObject.GetComponent<Animator>();
+        if (_anim == null)
+            Debug.LogError("Enemy animator is null");
         transform.position = new Vector3(MainCamera.randomX(2), MainCamera.CAMERA_LIMIT_VIEW.y, 0);
     }
 
@@ -26,7 +35,7 @@ public class Enemy : MonoBehaviour
         //if bottom of screen
         // respawn at top with a new random x position
         transform.Translate(Vector3.down * _speed * Time.deltaTime);
-        if (transform.position.y < -MainCamera.CAMERA_LIMIT_VIEW.y)
+        if (transform.position.y < -MainCamera.CAMERA_LIMIT_VIEW.y && !_isDestroyed)
             transform.position = new Vector3(MainCamera.randomX(2), MainCamera.CAMERA_LIMIT_VIEW.y, 0);
     }
 
@@ -36,24 +45,23 @@ public class Enemy : MonoBehaviour
 
         if (other.tag == "Player")
         {
-            Player player = other.transform.GetComponent<Player>();
-            if (player == null)
-            {
-                Debug.LogError("player is null");
-                Destroy(this.gameObject);
-                return;
-            }
-            player.damage();
-            Destroy(this.gameObject);
+            _player.damage();
+            _isDestroyed = true;
+            _speed=1.0f;
+            _anim.SetTrigger("OnEnemyDeath");
+            Destroy(this.gameObject,2.0f);
         }
         if (other.tag == "Laser")
         {
             Destroy(other.gameObject);
-            if(_player != null)
+            if (_player != null)
             {
                 _player.addScore(_points);
             }
-            Destroy(this.gameObject);
+            _speed=1.0f;
+            _isDestroyed=true;
+            _anim.SetTrigger("OnEnemyDeath");
+            Destroy(this.gameObject,2.0f);
         }
 
     }
